494abc80deb727a34d1df7e2e17c090869289388
VuTrongHanh99 <vuhanhcntt899@gmail.com>
2019-08-12 19:44:32 +0700

    processBar

--- SOURCE/app/src/main/java/com/linearlayout/chototapp/HomeFragment.java
+++ SOURCE/app/src/main/java/com/linearlayout/chototapp/HomeFragment.java
@@ -12,6 +12,7 @@ import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
@@ -38,6 +39,7 @@ public class HomeFragment extends Fragment {
     Category category;
     View vRoot;
     RecyclerView rvHomeFragment;
+    ProgressBar prLoading;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -46,66 +48,61 @@ public class HomeFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView( LayoutInflater inflater, ViewGroup container,
+                              Bundle savedInstanceState ) {
         // Inflate the layout for this fragment
-        vRoot = inflater.inflate(R.layout.fragment_home, container, false);
+        vRoot = inflater.inflate( R.layout.fragment_home, container, false );
         init();
         getdata();
         return vRoot;
     }
 
     void init() {
-        rvHomeFragment = vRoot.findViewById(R.id.rv_news);
+        rvHomeFragment = vRoot.findViewById( R.id.rv_news );
+        prLoading=vRoot.findViewById( R.id.pr_loading );
     }
 
     void getdata() {
-        final ProgressDialog progressDoalog;
-        progressDoalog = new ProgressDialog( getContext() );
-        progressDoalog.setMessage( "LOADING............." );
-        progressDoalog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-        progressDoalog.show();
+        prLoading.setVisibility( View.VISIBLE );
+//        final ProgressDialog progressDoalog;
+//        progressDoalog = new ProgressDialog( getContext() );
+//        progressDoalog.setMessage( "LOADING............." );
+//        progressDoalog.setProgressStyle( ProgressDialog.STYLE_SPINNER );
+//        progressDoalog.show();
 
-        Handler handler = new Handler(  );
-        handler.postDelayed( new Runnable() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .addConverterFactory( GsonConverterFactory.create() )
+                .baseUrl( "http://choviet247.vn/api/" )
+                .build();
+        retrofit.create( ChototService.class ).getListCategory().enqueue( new Callback<ResponseBody>() {
             @Override
-            public void run() {
-                Retrofit retrofit = new Retrofit.Builder()
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .baseUrl("http://choviet247.vn/api/")
-                        .build();
-                retrofit.create(ChototService.class).getListCategory().enqueue(new Callback<ResponseBody>() {
-                    @Override
-                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+            public void onResponse( Call<ResponseBody> call, Response<ResponseBody> response ) {
+                prLoading.setVisibility( View.INVISIBLE );
+                String strJson = null;
+                try {
+                    strJson = response.body().string();
+                    Gson gson = new Gson();
+                    category = gson.fromJson( strJson, Category.class );
+                    rvHomeFragment.setLayoutManager( new GridLayoutManager( getContext(), 2 ) );
+                    HomeAdapter adapter = new HomeAdapter();
+                    adapter.setContext( getContext() );
+                    adapter.setDatahome( category.data );
+                    rvHomeFragment.setAdapter( adapter );
+                    Toast.makeText( getContext(), "Đã lấy được dữ liệu", Toast.LENGTH_SHORT ).show();
+//                    progressDoalog.dismiss();
 
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
-                        String strJson = null;
-                        try {
-                            strJson = response.body().string();
-                            Gson gson = new Gson();
-                            category = gson.fromJson(strJson, Category.class);
-                            rvHomeFragment.setLayoutManager(new GridLayoutManager(getContext(), 2));
-                            HomeAdapter adapter = new HomeAdapter();
-                            adapter.setContext(getContext());
-                            adapter.setDatahome(category.data);
-                            rvHomeFragment.setAdapter(adapter);
-                            Toast.makeText(getContext(), "Đã lấy được dữ liệu", Toast.LENGTH_SHORT).show();
-                            progressDoalog.dismiss();
-
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-
-                    }
-
-                    @Override
-                    public void onFailure(Call<ResponseBody> call, Throwable t) {
-                        Toast.makeText(getContext(), "Không thể kết nối dữ liệu", Toast.LENGTH_SHORT).show();
-                        progressDoalog.dismiss();
-                    }
-                });
             }
-        } ,1000);
+
+            @Override
+            public void onFailure( Call<ResponseBody> call, Throwable t ) {
+                Toast.makeText( getContext(), "Không thể kết nối dữ liệu", Toast.LENGTH_SHORT ).show();
+                prLoading.setVisibility( View.INVISIBLE );
+            }
+        } );
 
 
     }
